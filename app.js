a570d6d799b52d891734909384fbb6efd4f87980
Piotr Delikat <piotrdellikat@gmail.com>
2018-03-15 00:05:34 +0100

    Added Procfile, deployment setup

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
-const router = require('./routes/api');
 const app = express();
 
-mongoose.connect('mongodb://<dbuser>:<password>@ds111319.mlab.com:11319/planet_advisor');
+const router = require('./routes/api');
+
+const dbUser = process.env.DB_USER
+const dbPassword = process.env.DB_PASSWORD
+mongoose.connect('mongodb://'+ dbUser +':' + dbPassword + '@ds111319.mlab.com:11319/planet_advisor');
 mongoose.Promise = global.Promise;
 
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use('/api', router)
 
 
-app.listen(process.env.PORT || 4000, function() {
-  console.log('App started on port 4000');
+app.listen(app.get('port'), function() {
+  console.log('App started on port ' + app.get('port'));
 });
